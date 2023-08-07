842f8cd434ef7d00848a6021224b4919a56dcf6c
Kateryna Laina <boba4ok@gmail.com>
2023-08-07 21:32:28 +0300

    feature: set up theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,44 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [],
+  content: ['./index.html', './src//*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '320px',
+      'md': '768px',
+      'lg': '1280px'
+    },
+    // fontSize: {
+    //   'heading-1': '44px',
+    //   'heading-2': '32px',
+    //   'heading-mob': '28px',
+    //   'heading-3': '24px',
+    //   'body-md': '20px',
+    //   'body-lg': '18px',
+    //   'body-default': '16px',
+    //   'body-sm': '14px',
+    //   'body-xs': '12px',
+    // },
+    colors: {
+      'black': "#151515",
+      'white': "#FCFCFC",
+      'yellow100': "#F7D67F",
+      'yellow50': "#F9E1A1",
+      'yellow30': "#FDF5E1",
+      'grey100': "#262626",
+      'grey50': "#808080",
+      'grey30': "#ECECEC",
+      'error100': "#D30018",
+      'error50': "#FE1F38",
+      'error30': "#FF9EA9",
+      'success100': "#7ED222"
+    },
+    backgroundColor: {
+      'whiteBg': '#ECECEC',
+    },
+    extend: {
+
+    },
   },
   plugins: [],
 }
