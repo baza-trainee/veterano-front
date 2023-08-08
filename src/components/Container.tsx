86829cb100a4b14b76c56e16951667510e316958
Kateryna Laina <boba4ok@gmail.com>
2023-08-08 16:12:13 +0300

    feature: create Container.tsx

---
+++ src/components/Container.tsx
@@ -0,0 +1,16 @@
+
+import {FC, ReactNode} from "react";
+
+interface ContainerProps {
+  children: ReactNode;
+}
+
+const Container: FC<ContainerProps> = ({children}) => {
+  return (
+    <div className="w-full">
+      {children}
+    </div>
+  );
+};
+
+export default Container;
