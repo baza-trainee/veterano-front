5cfd931af99d4592868508f11656866452996406
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-08-09 13:09:00 +0300

    feat: change breackpoint

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,13 +1,12 @@
-
-import {FC, ReactNode} from "react";
+import { FC, ReactNode } from "react";
 
 interface ContainerProps {
   children: ReactNode;
 }
 
-const Container: FC<ContainerProps> = ({children}) => {
+const Container: FC<ContainerProps> = ({ children }) => {
   return (
-    <div className="max-w-[480px] md:max-w-[768px] lg:max-w-[1440px] mx-auto">
+    <div className="max-w-[480px] md:max-w-[768px] xl:max-w-[1440px] mx-auto">
       {children}
     </div>
   );
