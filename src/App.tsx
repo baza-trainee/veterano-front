fa5663785774ee93b2ab8bce37782a3f9c1c3fc2
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-08-09 17:47:41 +0300

    cleared app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import Typography from "./components/Typography/Typography";
-
 function App() {
-  return (
-    <>
-      <Typography variant="h1">h1</Typography>
-      <Typography variant="h2">h2</Typography>
-      <Typography variant="h3">h3</Typography>
-      <Typography variant="h4">h4</Typography>
-      <Typography variant="h5">h5</Typography>
-      <Typography variant="p">p. body</Typography>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
