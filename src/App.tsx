076495578f874a0cc26aab1b9eb04c721f5cb3f4
Kateryna Laina <boba4ok@gmail.com>
2023-08-10 15:19:12 +0300

    feature: create ArrowButton component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import ArrowButton from "./components/ArrowButton.tsx";
+
 function App() {
-  return <></>;
+  return <div className={"m-9 flex w-[200px] justify-between "}>
+
+  <ArrowButton direction={'top'} disabled={false} variant={'faq'} onClick={() => console.log('clicked')}/>
+    <ArrowButton direction={'bottom'} disabled={false} variant={'faq'} onClick={() => console.log('clicked')}/>
+    <ArrowButton direction={'left'}   disabled={false} variant={'carousel'} onClick={() => console.log('clicked')}/>
+    <ArrowButton direction={'right'}  disabled={true} variant={'carousel'} onClick={() => console.log('clicked')}/>
+  </div>;
 }
 
 export default App;
