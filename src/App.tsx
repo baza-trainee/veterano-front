f73a10f07a04dd9f31b3f2cde608b00ffbff1361
Artem Shevchuk <art.shev00@gmail.com>
2023-08-15 09:55:47 +0200

    feature: added routing and Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Layout } from "./Layout/Layout";
+import { ErrorPage } from "./pages/ErrorPage";
+
 function App() {
-	return <></>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route path="*" element={<ErrorPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
