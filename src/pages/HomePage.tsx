8685bdbd20527faefee24c889b171d9d28dbb7f3
Kateryna Laina <boba4ok@gmail.com>
2023-08-15 16:36:49 +0300

    feature: ProjectSection done

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import ProjectSection from "../components/ProjectSection/ProjectSection.tsx";
+
+const HomePage = () => {
+	return (
+		<>
+		<ProjectSection/>
+		</>
+	);
+};
+
+export default HomePage;
