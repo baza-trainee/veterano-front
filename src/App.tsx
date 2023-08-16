d17668d367a98635069619fcc90dae523f8e11a9
Kateryna Laina <boba4ok@gmail.com>
2023-08-16 10:27:59 +0300

    fix: done

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import { Layout } from "./Layout/Layout";
 import { ErrorPage } from "./pages/ErrorPage";
 
 function App() {
+
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					<Route path="*" element={<ErrorPage />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route path="*" element={<ErrorPage />} />
+			</Route>
+		</Routes>
+	</BrowserRouter>
 	);
 }
 
