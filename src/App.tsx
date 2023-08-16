688b3e26d47956acb59d41fe8dcf55730afd32e1
Kateryna Laina <boba4ok@gmail.com>
2023-08-16 08:29:42 +0300

    feature: modal window done

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./Layout/Layout";
 import { ErrorPage } from "./pages/ErrorPage";
-import ModalWindow from "./components/Modal/ModalWindow.tsx";
 
 function App() {
-	return (
 
-<>
-	<ModalWindow/>
-	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<Layout />}>
-				<Route path="*" element={<ErrorPage />} />
-			</Route>
-		</Routes>
-	</BrowserRouter>
-</>
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Layout />}>
+						<Route path="*" element={<ErrorPage />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
 	);
 }
 
--- src/components/Modal/ModalWindow.tsx
+++ src/components/Modal/ModalWindow.tsx
@@ -1,9 +1,31 @@
+import { FC, ReactNode } from "react";
 
+interface ModalWindowProps {
+	className: string,
+	children?: ReactNode,
+	active: boolean,
+	setActive: React.Dispatch<React.SetStateAction<boolean>>,
+}
 
-const ModalWindow = () => {
+const ModalWindow: FC<ModalWindowProps> = ({ active, setActive, className, children }) => {
+
 	return (
-		<div className={''}>
 
+		<div
+			className={active ? "modal active" : "modal"}
+			onClick={() => setActive(false)}
+		>
+			<div
+				className={`${className} modal-content`}
+				onClick={(e) => e.stopPropagation()}
+			>
+				<div
+					onClick={() => setActive(false)}
+					className={"flex justify-end cursor-pointer"}>
+					<img src="/images/close.svg" alt="close" />
+				</div>
+				{children}
+			</div>
 		</div>
 	);
 };
