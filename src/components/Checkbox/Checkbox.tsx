778da30853c6b0fd39ea74e12b17072ddd42e70e
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-08-17 14:46:12 +0300

    feat: added checkbox

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,44 @@
+import React, { ChangeEvent, InputHTMLAttributes } from "react";
+import { BiCheck } from "react-icons/bi";
+
+interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
+	id: string;
+	label: string;
+	checked?: boolean;
+	onCheck?: (event: ChangeEvent<HTMLInputElement>) => void;
+	children?: React.ReactNode;
+}
+const Checkbox: React.FC<CheckboxProps> = ({
+	id = "test",
+	label,
+	checked = false,
+	onCheck = (event: ChangeEvent<HTMLInputElement>): void => {
+		console.log(event.target.checked);
+	},
+	children,
+	...props
+}) => {
+	return (
+		<>
+			<input
+				{...props}
+				type="checkbox"
+				id={id}
+				checked={checked}
+				onChange={onCheck}
+				className="hidden peer"
+			/>
+			<label htmlFor={id} className="flex gap-[20px] cursor-pointer peer ">
+				<BiCheck
+					fill={checked ? "black" : "transparent"}
+					className={
+						"block rounded-[4px] bg-yellow100 border-black border-[2px] w-[24px] h-[24px] " +
+						(!checked && " bg-[transparent]")
+					}
+				/>
+				{label || children}
+			</label>
+		</>
+	);
+};
+export default Checkbox;
