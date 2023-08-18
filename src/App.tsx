6c5757c820d296b7b5060686f223e1859f69cf62
Kateryna Laina <boba4ok@gmail.com>
2023-08-18 21:11:26 +0300

    feature: search 404 done

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ErrorPage } from "./pages/ErrorPage";
 import ContactUs from "./pages/ContactUs.tsx";
 import HomePage from "./pages/HomePage.tsx";
 import CookiesPanel from "./components/Cookies/CookiesPanel.tsx";
+import Search404 from "./components/Search404/Search404.tsx";
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
 					<Route path="/" element={<Layout />}>
 						<Route index element={<HomePage />} />
             <Route path="/contact" element={<ContactUs />} />
+						<Route path="/error" element={<Search404 />} />
 						<Route path="*" element={<ErrorPage />} />
 					</Route>
 				</Routes>
