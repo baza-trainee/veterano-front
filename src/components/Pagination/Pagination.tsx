d4c669204d97cbb857e677d40b32d4558725bd0a
Kateryna Laina <boba4ok@gmail.com>
2023-08-18 16:39:09 +0300

    feature: pagination doone

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,48 @@
+import { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import ArrowButton from "../ArrowButton/ArrowButton.tsx";
+import ReactPaginate from "react-paginate";
+
+interface PaginationProps {
+	pageCount: number,
+	currentPage: number,
+	onSelectedPage: (selectedPage: number) => void;
+}
+
+type PageClickData = {
+	selected: number;
+};
+
+const Pagination: FC<PaginationProps> = ({ pageCount = 10, currentPage = 1, onSelectedPage}) => {
+	const navigate = useNavigate();
+
+	const handlePageClick = (data: PageClickData) => {
+		const selectedPage = data.selected + 1;
+		navigate(`?page=${selectedPage}`);
+		onSelectedPage(selectedPage);
+	};
+
+	return (
+		<div className={'flex justify-end mr-[24px] lg:mr-[80px]'}>
+			<ReactPaginate
+				previousLabel={<ArrowButton direction={"left"} variant={'carousel'} disabled={currentPage === 1}/>}
+				nextLabel={<ArrowButton direction={"right"} variant={'carousel'} disabled={currentPage === pageCount}/>}
+				breakLabel={'...'}
+				breakClassName={'w-[28px] h-[28px] px-[10px] mr-[34px] flex items-center'}
+				pageCount={pageCount}
+				marginPagesDisplayed={1}
+				pageRangeDisplayed={3}
+				onPageChange={handlePageClick}
+				pageClassName={'w-[28px] h-[28px] px-[10px] rounded inline-flex items-center justify-end mr-[34px] hover:bg-yellow100 hover:text-white'}
+				containerClassName={'flex justify-between items-center text-[18px] font-light leading-[28px] mt-[50px] lg:mt-[59px] '}
+				activeClassName={'bg-black text-white'}
+				previousClassName={'previous order-2 mr-2 md:pl-[155px] lg:pl-[419px]'}
+				nextClassName={'order-3'}
+				forcePage={currentPage - 1}
+			/>
+		</div>
+
+	);
+};
+
+export default Pagination;
