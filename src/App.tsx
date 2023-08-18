1fe0a70a1c214327d4e503426dd12893b9f99adc
Kateryna Laina <boba4ok@gmail.com>
2023-08-18 21:15:26 +0300

    feature: remove page form routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { ErrorPage } from "./pages/ErrorPage";
 import ContactUs from "./pages/ContactUs.tsx";
 import HomePage from "./pages/HomePage.tsx";
 import CookiesPanel from "./components/Cookies/CookiesPanel.tsx";
-import Search404 from "./components/Search404/Search404.tsx";
 
 function App() {
 
@@ -15,7 +14,6 @@ function App() {
 					<Route path="/" element={<Layout />}>
 						<Route index element={<HomePage />} />
             <Route path="/contact" element={<ContactUs />} />
-						<Route path="/error" element={<Search404 />} />
 						<Route path="*" element={<ErrorPage />} />
 					</Route>
 				</Routes>
