07c4cb81c089ff59018689b29a891bd9b3eda6c9
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-08-21 15:27:06 +0300

    feat: added validation

--- src/pages/login/LoginForm.tsx
+++ src/pages/login/LoginForm.tsx
@@ -1,31 +1,66 @@
 import { ChangeEvent, useState } from "react";
 import Input from "../../components/Input/Input";
+import { string, object } from "yup";
+import { Formik, Form } from "formik";
 
 const LoginForm = () => {
 	const [login, setLogin] = useState<string>("");
 	const [password, setPassword] = useState<string>("");
+	const validationSchema = object({
+		login: string()
+			.email("Введіть дійсний email")
+			.test("domain", "Введіть дійсний email", (value) => {
+				return !value?.endsWith(".ru") && !value?.endsWith(".by");
+			})
+			.required("Введіть дійсний email"),
+		password: string()
+			.min(2, "Поля повинні мати більше 2 символів")
+			.max(52, "Ім’я повинно бути не більше 52 знаків")
+			.required("Заповніть пусте поле"),
+	});
+
 	return (
-		<form className="w-[518px]">
-			<Input
-				value={login}
-				type="text"
-				id="login"
-				label="Логін"
-				onChange={(e: ChangeEvent<HTMLInputElement>) => {
-					setLogin(e.target.value);
+		<>
+			<Formik
+				initialValues={{ name: "", email: "" }}
+				validationSchema={validationSchema}
+				onSubmit={async (values, { setSubmitting, resetForm }) => {
+					try {
+						sessionStorage.setItem("JWT", "JWT");
+						console.log(values);
+					} catch (e) {
+						console.log(e);
+					} finally {
+						setSubmitting(false);
+					}
+					resetForm();
 				}}
-			/>
-			<Input
-				name="password"
-				value={password}
-				type="password"
-				id="login"
-				label="Пароль"
-				onChange={(e: ChangeEvent<HTMLInputElement>) => {
-					setPassword(e.target.value);
-				}}
-			/>
-		</form>
+				validateOnChange={true}
+				validateOnBlur={true}
+			>
+				<Form className="w-[518px]">
+					<Input
+						value={login}
+						type="text"
+						id="login"
+						label="Логін"
+						onChange={(e: ChangeEvent<HTMLInputElement>) => {
+							setLogin(e.target.value);
+						}}
+					/>
+					<Input
+						name="password"
+						value={password}
+						type="password"
+						id="password"
+						label="Пароль"
+						onChange={(e: ChangeEvent<HTMLInputElement>) => {
+							setPassword(e.target.value);
+						}}
+					/>
+				</Form>
+			</Formik>
+		</>
 	);
 };
 
