d02648342f50501e04c6e2e9888f8d3fa81da38c
Kateryna Laina <boba4ok@gmail.com>
2023-08-22 17:24:56 +0300

    feature: admin panel start

--- src/Layout/AdminLayout.tsx
+++ src/Layout/AdminLayout.tsx
@@ -1,13 +1,19 @@
+import NavLinkAdmin from "../components/NavLinkAdmin/NavLinkAdmin.tsx";
 
 
 const AdminLayout = () => {
 	return (
-		<section>
-			<div>Logo</div>
-			<div>
-
+		<div className={'w-[272px] '}>
+			<header className={'bg-grey100'}>
+				<div className={'px-[32px] pt-[40px] pb-[38px]'}><img src="/images/logo-white-sm.svg" alt="" /></div>
+			</header>
+			<div className={'px-[32px] bg-yellow50 h-[100vh] flex flex-col gap-6'}>
+				<NavLinkAdmin icon={"/images/admin/folder.svg"} to={'/projects'}>Проєкти</NavLinkAdmin>
+				<NavLinkAdmin icon={"/images/admin/case.svg"} to={'/partners'}>Партнери</NavLinkAdmin>
+				<NavLinkAdmin icon={"/images/admin/contacts.svg"} to={'/contacts'}>Контакти</NavLinkAdmin>
+				<NavLinkAdmin icon={"/images/admin/file.svg"} to={'/documents'}>Документи</NavLinkAdmin>
 			</div>
-		</section>
+		</div>
 	);
 };
 
