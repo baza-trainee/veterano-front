9e501541822d7bb576ce8904884612c482a60522
Kateryna Laina <boba4ok@gmail.com>
2023-08-22 22:36:50 +0300

    feature: styling

--- src/components/AdminPanel/ListElements/ListElement.tsx
+++ src/components/AdminPanel/ListElements/ListElement.tsx
@@ -1,6 +1,8 @@
+import IconPen from "../IconButtons/IconPen.tsx";
+
 const ListElement = () => {
 	return (
-		<div className={"bg-white flex mx-10 mt-6 "}>
+		<div className={"bg-white flex mx-10 mt-6 items-center"}>
 			<div className={"w-[48px] h-[48px] p-3 check-wrapper "}>
 				<label className="custom-checkbox">
 					<input type="checkbox" className={'hidden'} />
@@ -10,10 +12,9 @@ const ListElement = () => {
 			<div>Назва проекту</div>
 			<div>активний</div>
 			<div>16.08.2023</div>
-			<div>
-				<div>
-				</div>
-				<div>
+			<div className={"flex"}>
+				<IconPen/>
+				<div className={"w-[36px] h-[36px] flex items-center justify-center "}>
 					<img src="/images/admin/bin.svg" alt="bin" />
 				</div>
 			</div>
