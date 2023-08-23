8b0f10834dd2249f612ebf48ac9ccf17f18a0bca
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-08-23 11:32:13 +0300

    Clear home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,13 @@
 import ProjectSection from "../components/ProjectSection/ProjectSection.tsx";
 import PartnersSection from "../components/PartnersSection/PartnersSection.tsx";
 import SubscribeSection from "../components/SubscribeSection/SubscribeSection.tsx";
-import ImageInput from "../components/ImageCroper/ImageInput.tsx";
 
 const HomePage = () => {
-
 	return (
 		<>
 			<ProjectSection />
 			<PartnersSection />
 			<SubscribeSection />
-			<ImageInput />
 		</>
 	);
 };
