7e4998dd2875a757f587d62284728312c7104bee
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-08-24 15:52:37 +0300

    ref: deleted test elems

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import ProjectSection from "../components/ProjectSection/ProjectSection.tsx";
 import PartnersSection from "../components/PartnersSection/PartnersSection.tsx";
 import SubscribeSection from "../components/SubscribeSection/SubscribeSection.tsx";
-import ImageInput from "../components/ImageCroper/ImageInput.tsx";
 
 const HomePage = () => {
 	return (
@@ -9,7 +8,6 @@ const HomePage = () => {
 			<ProjectSection />
 			<PartnersSection />
 			<SubscribeSection />
-			<ImageInput width={265} height={232} />
 		</>
 	);
 };
