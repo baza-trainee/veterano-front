5ee46c50a2bf1c98d705015ee0c6587cdf9934dd
Kateryna Laina <boba4ok@gmail.com>
2023-08-25 16:15:27 +0300

    feature: types for imageCrop

--- src/components/AdminPanel/BlobToBase64.ts
+++ src/components/AdminPanel/BlobToBase64.ts
@@ -1,4 +1,4 @@
-export const blobToBase64 = (blob) => {
+export const blobToBase64 = (blob: Blob) => {
 	return new Promise((resolve, reject) => {
 		const reader = new FileReader();
 		reader.onload = () => {
--- src/components/ImageCroper/ImageInput.tsx
+++ src/components/ImageCroper/ImageInput.tsx
@@ -2,29 +2,33 @@ import { FC, useEffect, useState } from "react";
 import { FileDrop } from "./FileDrop";
 import { ImageCroper } from "./ImageCroper";
 import { blobToBase64 } from "../AdminPanel/BlobToBase64.ts";
+
 interface ImageInput {
 	onSelectedImg: (preview: string) => void;
-
 }
+
 const ImageInput: FC<ImageInput> = ({
 onSelectedImg
 }) => {
 	const [isCropeningImg, setIsCropeningImg] = useState<boolean>(false);
 	const [preview, setPreview] = useState<string>("");
-	const [file, setFile] = useState<Blob>();
+	const [file, setFile] = useState<Blob | undefined>();
 
 	useEffect(() => {
-		if (preview !== "") {
+		if (preview !== "" && file) {
 			// Перетворення Blob на Base64
 			blobToBase64(file)
 				.then((base64String) => {
-					onSelectedImg(base64String);
+					if (typeof base64String === "string") {
+						onSelectedImg(base64String);
+					}
 				})
 				.catch((error) => {
 					console.error("Error:", error);
 				});
 		}
 	}, [preview, file, onSelectedImg]);
+
 	return (
 		<>
 			{isCropeningImg && file && (
