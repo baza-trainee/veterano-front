82d3ca8d0efcdcabaa1b5a492e7605a374bf445c
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-08-28 11:56:37 +0300

    fix: fixed layout

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -3,32 +3,35 @@ import Typography from "../Typography/Typography";
 import Container from "../Container/Container.tsx";
 
 interface SectionProps {
-  title: string;
-  className?: string;
-  children?: ReactNode;
+	title?: string;
+	className?: string;
+	children?: ReactNode;
 }
 
 const Section: FC<SectionProps> = ({
-  className = "",
-  title = "",
-  children
+	className = "",
+	title = "",
+	children,
 }) => {
-  return (
-    <section
-      className={
-        className + " " + "py-section-sm md:py-section-md lg:py-section-lg"
-      }
-    >
-      <Container>
-        <Typography variant="h2" className="text-center">
-          {title}
-        </Typography>
-        <div className="mt-section-content-sm md:mt-section-content-md lg:mt-section-content-lg">
-          {children}
-        </div>
-      </Container>
-    </section>
-  );
+	return (
+		<section
+			className={
+				className + " " + "py-section-sm md:py-section-md lg:py-section-lg"
+			}
+		>
+			<Container>
+				{title && (
+					<Typography
+						variant="h2"
+						className="text-center mb-section-content-sm md:mb-section-content-md lg:mb-section-content-lg"
+					>
+						{title}
+					</Typography>
+				)}
+				<div className="">{children}</div>
+			</Container>
+		</section>
+	);
 };
 
 export default Section;
