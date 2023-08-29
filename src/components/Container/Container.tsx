823587d2989f1dd375d6bc58bb5d1863ae76e344
Kateryna Laina <boba4ok@gmail.com>
2023-08-29 15:53:46 +0300

    fix: fix styles in Container to fix displaying dropdown on mobile

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -9,7 +9,7 @@ const Container: FC<ContainerProps> = ({ children, className, ...props }) => {
 	return (
 		<div
 			className={
-				"max-w-[480px] md:max-w-[768px] lg:max-w-[1440px] mx-auto" +
+				"max-w-[480px] md:max-w-[768px] lg:max-w-[1440px] md:mx-auto" +
 				" " +
 				className
 			}
