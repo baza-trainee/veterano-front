070bd77016966c73c756f4c47d2510c98ddc88a2
Artem Shevchuk <art.shev00@gmail.com>
2023-09-01 14:06:53 +0200

    changed style

--- src/ruleFiles/PrivacyPolicy.tsx
+++ src/ruleFiles/PrivacyPolicy.tsx
@@ -3,12 +3,10 @@ import Container from "../components/Container/Container";
 
 export const PrivacyPolic = () => {
 	return (
-		<Container>
-			<embed
-				src={"/files/pol_conf.pdf" + "#toolbar=0"}
-				type="application/pdf"
-				className="w-full h-screen border-none"
-			/>
-		</Container>
+		<embed
+			src={"/files/pol_conf.pdf" + "#toolbar=0"}
+			type="application/pdf"
+			className="w-full h-screen border-none"
+		/>
 	);
 };
--- src/ruleFiles/TermsSite.tsx
+++ src/ruleFiles/TermsSite.tsx
@@ -2,12 +2,10 @@ import Container from "../components/Container/Container";
 
 export const TermsSite = () => {
 	return (
-		<Container>
-			<embed
-				src={"/files/site_rules.pdf" + "#toolbar=0"}
-				type="application/pdf"
-				className="w-full h-screen border-none"
-			/>
-		</Container>
+		<embed
+			src={"/files/site_rules.pdf" + "#toolbar=0"}
+			type="application/pdf"
+			className="w-full h-screen border-none"
+		/>
 	);
 };
