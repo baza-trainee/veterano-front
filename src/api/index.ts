b5b3c20402a7cbd472abe90cb6517a11627c8302
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-09-02 09:05:23 +0300

    change log

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-console.log(import.meta.env.VITE_BASE_URL);
+console.table(import.meta.env);
 const $host = axios.create({
 	baseURL: import.meta.env.VITE_BASE_URL,
 });
