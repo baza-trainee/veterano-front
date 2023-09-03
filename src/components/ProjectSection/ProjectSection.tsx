041158719141920651241e434516069eeb96b5fb
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-09-03 23:56:18 +0300

    fix: fixed: Project card size, section layout

--- src/components/ProjectSection/ProjectSection.tsx
+++ src/components/ProjectSection/ProjectSection.tsx
@@ -24,10 +24,7 @@ const ProjectSection = () => {
 	}, []);
 
 	return (
-		<Section
-			title={"Проєкти"}
-			className={"bg-grey30 md:pl-[38px] md:pr-[10px] lg:px-[80px]"}
-		>
+		<Section title={"Проєкти"} className={"bg-grey30"}>
 			{isDesktop && items?.length && (
 				<Carousel
 					items={items || []}
