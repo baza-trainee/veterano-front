af79b5212901b372f2e115f2f9921ddc659ebc42
Kateryna Laina <boba4ok@gmail.com>
2023-09-05 11:13:38 +0300

    fix: new tab view of cookies panel

--- src/components/Cookies/CookiesPanel.tsx
+++ src/components/Cookies/CookiesPanel.tsx
@@ -24,16 +24,16 @@ const CookiesPanel = () => {
 		showPanel && (
 			<div
 				id="cookie-panel"
-				className="fixed bottom-0 left-0 right-0 bg-[#F9F6EC] py-8 px-4 flex flex-col justify-center gap-6 md:px-6 md:py-6 lg:px-[80px] lg:py-4 md:gap-9 lg:flex-row lg:justify-between lg:min-w-[1440px] lg:items-center  ">
-					<div className={'flex justify-end flex-shrink-0 lg:order-3'}>
+				className="fixed bottom-0 left-0 right-0 bg-[#F9F6EC] py-8 px-4 flex flex-col justify-center gap-6 md:h-[166px] md:px-6 md:py-6 lg:px-[80px] lg:h-[104px] lg:py-4 md:flex-row md:justify-between lg:min-w-[1440px] md:items-center  ">
+					<div className={'flex justify-end flex-shrink-0 md:order-3'}>
 						<CloseBtn onClick={() => setShowPanel(!showPanel)}/>
 					</div>
 
-					<p className="text-[16px] leading-6 lg:order-1 lg:min-w-[845px]">
+					<p className="text-[16px] leading-6 font-light md:w-[457px] md:order-1 lg:min-w-[845px]">
 						Цей сайт використовує файли cookies для правильної роботи і покращення сервісу. Якщо ви погоджуєтесь з їхнім
 						використанням, натисніть ОК. Більше інформації в <Link to={'/files/pol_conf.pdf'} variant={'underlineFooter'} style={{color: 'black'}}>Політика конфіденційності</Link>.
 					</p>
-				<div className="w-full flex justify-center md:w-[95px] lg:w-[93px] lg:order-2">
+				<div className="w-full flex justify-center md:w-[95px] lg:w-[93px] md:order-2">
 					<Button id="accept-cookies" variant={'primary'} size={'large'} onClick={acceptCookies} className={'py-[10px] h-[48px]'}>OK</Button>
 				</div>
 			</div>
