5ea0942930006574e51b889d29a37a2f2e205c0c
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-09-09 14:10:42 +0300

    fix types

--- src/components/PartnersCard/PartnersCard.tsx
+++ src/components/PartnersCard/PartnersCard.tsx
@@ -6,16 +6,17 @@ interface PartnersCardProps {
 }
 const PartnersCard: FC<PartnersCardProps> = ({ url, image }) => {
 	return (
-		<a href={url}>
-			<div
-				style={{
-					backgroundImage: `url(${image})`,
-					backgroundPosition: "center",
-					backgroundSize: "cover",
-				}}
-				className={"w-full h-[124px] md:h-[72px] lg:h-[94px]"}
-			></div>
-		</a>
+		<a
+			href={url}
+			style={{
+				backgroundImage: `url(${
+					import.meta.env.VITE_BASE_URL
+				}/search/image/get?id=${image})`,
+				backgroundPosition: "center",
+				backgroundSize: "cover",
+			}}
+			className={"w-full h-[124px] md:h-[72px] lg:h-[94px]"}
+		></a>
 	);
 };
 
