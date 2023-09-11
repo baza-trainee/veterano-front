c3448ce0a8c88eb6a156abfddd5a2e4bb12193a4
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-09-11 17:27:28 +0300

    added get to contacts form

--- src/pages/adminPage/contacts/Contacts.tsx
+++ src/pages/adminPage/contacts/Contacts.tsx
@@ -5,70 +5,78 @@ import AdminInput from "../../../components/AdminPanel/Input/AdminInput";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import { validationSchema } from "./schema.ts";
 import { submitForm } from "./submit.ts";
+import { useContacts } from "../../../hooks/useContacts.ts";
 
 export const Contacts = () => {
+	const contacts = useContacts();
 	return (
 		<div>
 			<AdminHeader name="Контакти" />
 			<div className="pl-9 pr-[80px] pt-12  ">
-				<Formik
-					validationSchema={validationSchema}
-					initialValues={{ phone: "", secondPhone: "", email: "" }}
-					onSubmit={(values, { resetForm }) => {
-						submitForm(values);
-						resetForm();
-					}}
-				>
-					{() => (
-						<Form>
-							<div className="flex justify-between mb-[18px]">
-								<Typography variant="h2" className="font-medium text-2xl">
-									Редагувати контакти
-								</Typography>
-								<div className="hidden md:block">
+				{contacts && (
+					<Formik
+						validationSchema={validationSchema}
+						initialValues={{
+							phone: contacts?.firstPhoneNumber || "",
+							secondPhone: contacts?.secondPhoneNumber || "",
+							email: contacts?.email || "",
+						}}
+						onSubmit={(values, { resetForm }) => {
+							submitForm(values);
+							resetForm();
+						}}
+					>
+						{() => (
+							<Form>
+								<div className="flex justify-between mb-[18px]">
+									<Typography variant="h2" className="font-medium text-2xl">
+										Редагувати контакти
+									</Typography>
+									<div className="hidden md:block">
+										<Button variant="primary" type="submit">
+											Опублікувати
+										</Button>
+									</div>
+								</div>
+								<div className="flex flex-col gap-4 max-w-[739px]">
+									<Field as={AdminInput} placeholder="Телефон" name="phone" />
+									<ErrorMessage
+										name="phone"
+										component="div"
+										className="text-red-500"
+									/>
+
+									<Field
+										as={AdminInput}
+										placeholder="Телефон"
+										name="secondPhone"
+									/>
+									<ErrorMessage
+										name="secondPhone"
+										component="div"
+										className="text-red-500"
+									/>
+
+									<Field
+										as={AdminInput}
+										placeholder="Контактний e-mail"
+										name="email"
+									/>
+									<ErrorMessage
+										name="email"
+										component="div"
+										className="text-red-500"
+									/>
+								</div>
+								<div className="block md:hidden mt-4">
 									<Button variant="primary" type="submit">
 										Опублікувати
 									</Button>
 								</div>
-							</div>
-							<div className="flex flex-col gap-4 max-w-[739px]">
-								<Field as={AdminInput} placeholder="Телефон" name="phone" />
-								<ErrorMessage
-									name="phone"
-									component="div"
-									className="text-red-500"
-								/>
-
-								<Field
-									as={AdminInput}
-									placeholder="Телефон"
-									name="secondPhone"
-								/>
-								<ErrorMessage
-									name="secondPhone"
-									component="div"
-									className="text-red-500"
-								/>
-
-								<Field
-									as={AdminInput}
-									placeholder="Контактний e-mail"
-									name="email"
-								/>
-								<ErrorMessage
-									name="email"
-									component="div"
-									className="text-red-500"
-								/>
-							</div>
-							<div className="block md:hidden mt-4">
-								<Button variant="primary" type="submit">
-									Опублікувати
-								</Button>
-							</div>
-						</Form>
-					)}
-				</Formik>
+							</Form>
+						)}
+					</Formik>
+				)}
 			</div>
 		</div>
 	);
