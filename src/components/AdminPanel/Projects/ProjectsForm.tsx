52091edfed21ee8d30122b9fb9b71095f39f4f53
Kateryna Laina <boba4ok@gmail.com>
2023-09-11 12:25:29 +0300

    fix: implement navigate to prev page after additnig and adding parners and projects

--- src/components/AdminPanel/Projects/ProjectsForm.tsx
+++ src/components/AdminPanel/Projects/ProjectsForm.tsx
@@ -73,12 +73,8 @@ const ProjectsForm: FC<ProjectsFormProps> = ({
 			onSubmit={async (values, { setSubmitting }) => {
 				try {
 					if (values && type === "add") {
-						// const { isEnabled, city, country, category, image, ...rest } = values;
 						const { isEnabled, location, category, image, ...rest } = values;
-
-						// const location = { city, country };
 						let locationObject;
-
 						if (typeof location === "string") {
 							const locationArray = location.split("/");
 							locationObject = { city: locationArray[0].trim(), country: locationArray[1].trim() };
@@ -93,9 +89,8 @@ const ProjectsForm: FC<ProjectsFormProps> = ({
 							location: locationObject,
 							categories: categoryArray,
 						};
-						console.log(cardData);
 						createCard(cardData)
-							.then(() => navigate("/admin/projects"));
+							.then(() => navigate(-1));
 					} else {
 
 						const changedValues = Object.keys(initialState).reduce<Partial<ProjectsFormProps>>((acc, key) => {
@@ -122,10 +117,9 @@ const ProjectsForm: FC<ProjectsFormProps> = ({
 							location: locationObject,
 							categories: categoryArray,
 						};
-						console.log(cardData);
 						editCard(cardData)
 							.then(() => {
-								navigate("/admin/projects");
+								navigate(-1)
 							})
 							.catch((error) => {
 								console.error("Помилка при запиті:", error);
